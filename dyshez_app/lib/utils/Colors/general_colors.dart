a9a67314d0033b46806bab1be67d4da91c3aae27
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2024-06-24 16:22:22 -0700

    ui Colorimetry

---
+++ dyshez_app/lib/utils/Colors/general_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color pink = Color(0xFFE3026F);
+const Color pinkLinks = Color(0xFFFA4B8B);
+const Color redCancel = Color(0xFFDC2B2B);
+const Color greenSuccess = Color(0xFF11845B);
+const Color darkBlue = Color(0xFF262338);
+const Color grayColor = Color(0xFFE7E7E9);
+const Color opaqueGrayColor = Color(0xFF818181);
+const Color grayBoldColor = Color(0xFF747474);
+const Color arrowGrayColor = Color(0xFF484747);
+const Color supportGrayColor = Color(0xFF818181);
+const Color questionGrayColor = Color(0xFF4D4D4D);
+const Color black = Color(0xFF0C0C0C);
+const Color white = Color(0xFFFFFFFF);
+const Color opaqueWhiteBackground = Color(0xFFFAFAFA);
